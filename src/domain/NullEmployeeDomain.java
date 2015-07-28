5fe4c5b7bad387090b481e8e387330ca9afe4775
naotake.k <naotake.87@gmail.com>
2015-07-28 22:55:10 +0900

    ドメイン層のクラスに JavaDoc を追加

--- src/domain/NullEmployeeDomain.java
+++ src/domain/NullEmployeeDomain.java
@@ -2,6 +2,11 @@ package domain;
 
 import entity.Employee;
 
+/**
+ * {@link EmployeeDomain}のヌルオブジェクト。
+ *
+ * @author naotake
+ */
 public class NullEmployeeDomain extends EmployeeDomain {
 
     private static final NullEmployeeDomain NULL = new NullEmployeeDomain();
@@ -10,14 +15,25 @@ public class NullEmployeeDomain extends EmployeeDomain {
         // NOP
     }
 
+    /**
+     * @return 空文字
+     */
     public String getNo() {
         return "";
     }
 
+    /**
+     * @return NULL を返す
+     */
     public Employee getEntity() {
         return null;
     }
 
+    /**
+     * シングルトンのオブジェクトを取得する。
+     *
+     * @return ヌルオブジェクト
+     */
     public static NullEmployeeDomain getInstance() {
         return NULL;
     }
